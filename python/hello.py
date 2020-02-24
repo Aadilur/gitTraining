bb2748f2a9bd8083c0f51a1735cfbfe08553a071
Aadilur <hardnut103@gmail.com>
2020-02-24 18:54:25 +0600

    all

--- python/hello.py
+++ python/hello.py
@@ -1,1 +1,11 @@
-print("hello!")
+name = "Adil"
+print(f"Hello, {name} !")
+
+x = -9
+
+if x > 0:
+    print("Value of X is positive")
+elif x < 0:
+    print("Value of X is negative")
+else:
+    print("Value of X is zero")
